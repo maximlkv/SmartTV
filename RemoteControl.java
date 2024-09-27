805ec9ae7853003694a5cf3ba3ed854e6b5d43a2
maximlkv <maxim.malkov@stud.hs-hannover.de>
2024-09-27 11:29:11 +0200

    refactoring of RemoteControl

--- RemoteControl.java
+++ RemoteControl.java
@@ -22,7 +22,7 @@ public class RemoteControl {
             SERVER_HOST = args[0];
             TCP_PORT = Integer.valueOf(args[1]);
         } else {
-            System.err.println("Please provide hostname and Port number as command line parameters, in order to connect to a SmartTV.");
+            System.err.println("Please provide (only) hostname and Port number as command line parameters, in order to connect to a SmartTV.");
         }
         RemoteControl remoteControl = new RemoteControl();
         remoteControl.run();
@@ -35,20 +35,50 @@ public class RemoteControl {
                 do {
                     buttonNumber = remoteInputs.next();
                     socketWriter.println(buttonNumber);
-                    String serverResponse;
-                    while ((serverResponse = reiceiveOneLineFromServer()) != null) {
-                        if (serverResponse.equals("END_OF_MESSAGE")) {
-                            break;
-                        }
-                        System.out.println(serverResponse);
-                    }
+                    getServerMessage();
                 } while (true); 
             } catch (Exception e) {
                 System.err.println(e.getMessage());
             }
         }
-        
         disconnect();
+    }
+
+    private boolean connect() {
+        boolean success = false;
+        try {
+            socket = new Socket(SERVER_HOST, TCP_PORT);
+            remoteInputs = new Scanner(System.in);
+            socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            socketWriter = new PrintWriter(socket.getOutputStream(), true);
+            System.out.println("Connection established");
+            success = true;
+            getServerMessage();
+        } catch (IOException e) {
+            System.err.println("Could not connect to the server: " + e.getMessage());
+        }
+
+        return success;
+    }
+
+    private void getServerMessage() {
+        String serverResponse;
+        while ((serverResponse = reiceiveOneLineFromServer()) != null) {
+            if (serverResponse.equals("END_OF_MESSAGE")) {
+                break;
+            }
+            System.out.println(serverResponse);
+        }
+    }
+
+    private String reiceiveOneLineFromServer() {
+        String response = null;
+        try {
+            response = socketReader.readLine();
+        } catch (IOException e) {
+            System.err.println("Error receiving data from the server: " + e.getMessage());       
+        }        
+        return response;
     }
 
     /**
@@ -67,37 +97,8 @@ public class RemoteControl {
         }
     }
 
-    private boolean connect() {
-        boolean success = false;
-        try {
-            socket = new Socket(SERVER_HOST, TCP_PORT);
-            remoteInputs = new Scanner(System.in);
-            socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            socketWriter = new PrintWriter(socket.getOutputStream(), true);
-            System.out.println("Connection established");
-            success = true;
-            String serverResponse;
-            while ((serverResponse = reiceiveOneLineFromServer()) != null) {
-                if (serverResponse.equals("END_OF_MESSAGE")) {
-                    break;
-                }
-                System.out.println(serverResponse);
-            }
-        } catch (IOException e) {
-            System.err.println("Could not connect to the server: " + e.getMessage());
-        }
+    
 
-        return success;
-    }
-
-    private String reiceiveOneLineFromServer() {
-        String response = null;
-        try {
-            response = socketReader.readLine();
-        } catch (IOException e) {
-            System.err.println("Error receiving data from the server: " + e.getMessage());       
-        }        
-        return response;
-    }
+    
 
 }
